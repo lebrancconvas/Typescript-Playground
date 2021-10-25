95ed364276078fd2c0c1d29bbf0c86ac4d03f3e5
lebrancconvas <lebrancconvas@gmail.com>
2021-10-26 06:57:34 +0700

    ✨ Add More Concept about  Typescript.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 //Typescript uses Language Server Protocal to provide the compiler. 
-const add = (a, b) => a + b; // add(a: any, b: any) => any 
+const add = (a: number, b: number) => a + b; // add(a: any, b: any) => any 
 add(1, 2); 
 
 //VSCode always use "Typescript Compiler". 
@@ -34,10 +34,32 @@ console.log(catBark('MeowMeow')); //Argument of type '"MeowMeow"' is not assigna
 const flip = (n: true) => !n; 
 console.log(flip(true));
 
-const aggresive: (arg0: string) => any (mood: 'angry'): String => `I am ${mood}`;
-console.log(aggresive('happy'));
+const aggresive = (mood: string) => `I am ${mood}`;
+console.log(aggresive('happy')); 
 
 const sudoku: Number[] = [1,2,3,4,5];
 const sudokuGrid = [[1,2], [3,"Bingo"]]; 
-const isCat = cat => !cat; 
+const isCat = (cat: boolean) => !cat;  
 const is5: (true | 5)[][] = [[true, 5], [5, true]]; 
+
+const [counter, setCounter] = useEffect();
+
+function useEffect(): [number, () => void] {
+	return [1024, () => {}]; 
+} 
+
+function adder1(a: any, b: any) {
+	a.useState();
+}
+
+function adder2(a: unknown, b: unknown) {
+	a.useState();
+}
+
+function noReturn(): void {
+	//return 1024; 
+}
+
+function noMoreReturn(Message: string): never {
+	throw new Error(Message); 
+}
